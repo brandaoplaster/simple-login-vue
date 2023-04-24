00d726becaf2175e1d1433b7ff8d057bdeb2aae9
brandaoplaster <brandaoplaster@gmail.com>
2023-04-24 15:36:03 -0300

    feat: adding components

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <header class="jumbotron">
+      <h3>Welcome, you are on the dashboard and are logged in with {{currentUser.email}}</h3>
+    </header>
+  </div>
+</template>
+
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Dashboard",
+  data() {
+    return {
+      content: "",
+    };
+  },
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user.data;
+    }
+  },
+    mounted() {
+    if (!this.currentUser) {
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <header class="jumbotron">
+      <h3>welcome you are at Home</h3>
+    </header>
+  </div>
+</template>
+
+<script>
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Home",
+  data() {
+    return {
+      content: "",
+    };
+  }
+};
+</script>
